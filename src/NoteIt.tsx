4ff3def336f504fe50076014d94957e0bb5e650a
liphenste <liphenste@gmail.com>
2021-06-27 16:27:23 -0400

    add noteit

--- src/NoteIt.tsx
+++ src/NoteIt.tsx
@@ -3,7 +3,22 @@ import Button from 'react-bootstrap/Button';
 import './NoteIt.css';
 
 export default function NoteIt(): ReactElement {
+    const activateExtension = async () => {
+        let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+        chrome.scripting.executeScript({
+            target: { tabId: tab.id || -1 },
+            function: setPageBackgroundColor,
+        });
+    }
+
+    const setPageBackgroundColor = () => {
+        chrome.storage.sync.get("color", ({ color }) => {
+            document.body.style.backgroundColor = color;
+        });
+    }
+
     return (
-        <Button variant="primary" className="NoteIt">Note it!</Button>
+        <Button variant="primary" className="NoteIt" onClick={activateExtension}>Note it!</Button>
     )
 }
