6e8d2b10db362b6976f14f452510d40ce6774f39
liphenste <liphenste@gmail.com>
2021-11-02 00:54:09 -0400

    update redirect_uri clientside

--- src/SignIn.tsx
+++ src/SignIn.tsx
@@ -4,7 +4,7 @@ import './SignIn.css';
 
 export default function SignIn(): ReactElement {
     const client_id = "0c4d2e6e-0a7d-4f3b-98ce-a230301477a8";
-    const redirect_uri = "https%3A%2F%2Fnorthamerica-northeast1-studious-bit-325802.cloudfunctions.net%2Fnotion-auth";
+    const redirect_uri = "https%3A%2F%2Fus-central1-studious-bit-325802.cloudfunctions.net%2Fnotion-auth";
     const href = `https://api.notion.com/v1/oauth/authorize?response_type=code&client_id=${client_id}&redirect_uri=${redirect_uri}&owner=user`;
 
     return (
